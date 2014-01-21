442d640dfd829ecefd0c9098a130f7df1aeb29f5
Nicklas Utgaard <nutgaard@gmail.com>
2014-01-21 15:48:17 +0100

    Removed some comments

--- Application/src/main/java/no/utgdev/kerbal/security/PluginPolicy.java
+++ Application/src/main/java/no/utgdev/kerbal/security/PluginPolicy.java
@@ -20,14 +20,9 @@ public class PluginPolicy extends Policy {
 
     @Override
     public PermissionCollection getPermissions(CodeSource codeSource) {
-        Certificate[] certs = codeSource.getCertificates();       
-        CodeSigner[] signers = codeSource.getCodeSigners();
-        System.out.println("Creating permissions for: "+codeSource.getLocation().toString());
-        System.out.println("Cert length: "+certs+" "+signers);
         Permissions p = new Permissions();
         p.add(new PropertyPermission("WebLookAndFeel.*", "read"));
         p.add(new RuntimePermission("getClassLoader"));
-//        p.add(new LoggingPermission("control", null));
         return p;
     }
 
