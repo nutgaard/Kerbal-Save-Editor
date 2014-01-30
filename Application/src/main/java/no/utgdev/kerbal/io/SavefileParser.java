6e5cc0c4b3440a6fde5f83d6de79eec0ffcd2c7a
Nicklas Utgaard <nutgaard@gmail.com>
2014-01-30 16:51:06 +0100

    Added proper handling of malformed savefiles

--- Application/src/main/java/no/utgdev/kerbal/io/SavefileParser.java
+++ Application/src/main/java/no/utgdev/kerbal/io/SavefileParser.java
@@ -4,8 +4,10 @@
  */
 package no.utgdev.kerbal.io;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import no.utgdev.kerbal.common.i18n.I18N;
 import no.utgdev.kerbal.common.treemodel.MarshallingObjectFactory;
 import no.utgdev.kerbal.common.treemodel.Property;
 import no.utgdev.kerbal.common.treemodel.PropertyMap;
@@ -42,53 +44,65 @@ public class SavefileParser {
     }
 
     public Savefile parse() {
-        logger.debug("Starting parsing of {}", this.name);
-        String current = null;
+        try {
+            logger.debug("Starting parsing of {}", this.name);
+            String current = null;
 
-        for (int i = 0, l = content.size(); i < l; i++) {
-            current = content.poll().trim();
+            for (int i = 0, l = content.size(); i < l; i++) {
+                current = content.poll().trim();
 
-            if (content.peek() != null && content.peek().trim().equals("{")) {
-                process(ParserEvents.TAG, current);
-            } else if (current.equals("{")) {
-                process(ParserEvents.OPENINGBRACKET, current);
-            } else if (current.equals("}")) {
-                process(ParserEvents.CLOSINGBRACKET, current);
-            } else {
-                String[] keyvalue = current.split("=");
-                String[] pkv = new String[2];
-                pkv[0] = (keyvalue.length >= 1 && keyvalue[0] != null) ? keyvalue[0] : "";
-                pkv[1] = (keyvalue.length == 2 && keyvalue[1] != null) ? keyvalue[1] : "";
-                process(ParserEvents.KEYVALUE, pkv[0], pkv[1]);
+                if (content.peek() != null && content.peek().trim().equals("{")) {
+                    process(ParserEvents.TAG, current);
+                } else if (current.equals("{")) {
+                    process(ParserEvents.OPENINGBRACKET, current);
+                } else if (current.equals("}")) {
+                    process(ParserEvents.CLOSINGBRACKET, current);
+                } else {
+                    String[] keyvalue = current.split("=");
+                    String[] pkv = new String[2];
+                    pkv[0] = (keyvalue.length >= 1 && keyvalue[0] != null) ? keyvalue[0] : "";
+                    pkv[1] = (keyvalue.length == 2 && keyvalue[1] != null) ? keyvalue[1] : "";
+                    process(ParserEvents.KEYVALUE, pkv[0], pkv[1]);
+                }
             }
+            logger.debug("Parsing of {} completed.", this.name);
+            logger.debug("Residuals: ");
+            logger.debug("\t{}", currentTag);
+            logger.debug("\t{}", Arrays.toString(currentPropertyMap.toArray()));
+        } catch (Exception e) {
+            logger.error("An error accured while parsing " + name + ". Returning empty savefile.", e);
+            return new Savefile(I18N.getInstance().getString("nofile"));
         }
-        logger.debug("Parsing of {} completed.", this.name);
         return savefile;
     }
 
-    private void process(ParserEvents event, String... strings) {
-        switch (event) {
-            case TAG:
-                if (strings.length == 0 || strings[0] == null) {
-                    throw new IllegalArgumentException("Tag string null");
-                }
-                this.currentTag = strings[0].trim();
-                break;
-            case OPENINGBRACKET:
-                this.currentPropertyMap.push(MarshallingObjectFactory.create(currentTag));
-                currentTag = null;
-                break;
-            case CLOSINGBRACKET:
-                PropertyMap pm = this.currentPropertyMap.pop();
-                this.currentPropertyMap.peek().addChild(pm);
-                logger.trace("{}: {}", this.currentPropertyMap.peek().getName(), pm.getName());
-                break;
-            case KEYVALUE:
-                logger.trace("{}: {}->{}", this.currentPropertyMap.peek().getName(), strings[0], strings[1]);
-                this.currentPropertyMap.peek().addChild(new Property(strings[0].trim(), strings[1].trim()));
-                break;
-            default:
-                throw new IllegalArgumentException("Fuck");
+    private void process(ParserEvents event, String... strings) throws IllegalArgumentException {
+        try {
+            switch (event) {
+                case TAG:
+                    if (strings.length == 0 || strings[0] == null) {
+                        throw new IllegalArgumentException("Tag string null");
+                    }
+                    this.currentTag = strings[0].trim();
+                    break;
+                case OPENINGBRACKET:
+                    this.currentPropertyMap.push(MarshallingObjectFactory.create(currentTag));
+                    currentTag = null;
+                    break;
+                case CLOSINGBRACKET:
+                    PropertyMap pm = this.currentPropertyMap.pop();
+                    this.currentPropertyMap.peek().addChild(pm);
+                    logger.trace("{}: {}", this.currentPropertyMap.peek().getName(), pm.getName());
+                    break;
+                case KEYVALUE:
+                    logger.trace("{}: {}->{}", this.currentPropertyMap.peek().getName(), strings[0], strings[1]);
+                    this.currentPropertyMap.peek().addChild(new Property(strings[0].trim(), strings[1].trim()));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid parserEvent: " + event);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("An error accured while processing a ParseEvent: " + event, e);
         }
     }
 }
